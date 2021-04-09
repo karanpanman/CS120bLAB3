3fb6353ed57ce269a0fb55a8ef88d1b5803490a7
Karan Bhogal <kbhog001@wch136-24.cs.ucr.edu>
2021-04-08 17:38:31 -0700

    Completed part 5

---
+++ kbhog001_lab3_part5.c
@@ -0,0 +1,40 @@
+/*	Author: Karan Bhogal
+ *  Partner(s) Name: 
+ *	Lab Section: 21
+ *	Assignment: Lab #3  Exercise #4
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRD = 0x000; PORTD = 0xFFF;
+	DDRB = 0xFFE; PORTB = 0x001;
+	unsigned char tmpD = 0x000;
+	unsigned char tmpB = 0x00;
+
+
+    /* Insert your solution below */
+    while (1) {
+	tmpD = PIND;
+	tmpB = 0x000;
+ 	tmpD = tmpD << 1;
+	tmpD = tmpD | PINB;
+	
+	if ( tmpD >= 70){
+		tmpB = tmpB | 0x002;
+	}
+	else if (tmpD > 5){
+		tmpB = tmpB | 0x004;
+	}
+
+	PORTB = tmpB;
+    }
+    return 1;
+}
